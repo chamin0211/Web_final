34cfd760a3b5e8d0d3704595a90c7cb809324822
Mingyu Cha <108313761+chamin0211@users.noreply.github.com>
2023-12-13 15:21:47 +0900

    Add files via upload

---
+++ WebProject-main/WebProject-main/board/demo/WebProject-main/WebProject-main/board/demo/src/main/java/com/example/demo/dto/AccountDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AccountDto {
+
+    private String username;
+    private String password;
+
+}
